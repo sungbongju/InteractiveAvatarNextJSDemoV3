f9a4c181c407496937aac83b2554cfab6f56c7da
sdkparkforbi <sdkparkforbi@gmail.com>
2025-12-30 17:33:35 +0900

    Update constants.ts

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -3,4 +3,20 @@ export const AVATARS = [
     avatar_id: "Graham_Chair_Sitting_public",
     name: "두뇌 건강 도우미",
   },
+  {
+    avatar_id: "Ann_Therapist_public",
+    name: "Ann Therapist",
+  },
+  {
+    avatar_id: "Shawn_Therapist_public",
+    name: "Shawn Therapist",
+  },
+  {
+    avatar_id: "Bryan_FitnessCoach_public",
+    name: "Bryan Fitness Coach",
+  },
+  {
+    avatar_id: "Elenora_IT_Sitting_public",
+    name: "Elenora Tech Expert",
+  },
 ];

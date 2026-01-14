c9b8e259d7a5a6e05a1bbb05c0c2b52b3b820211
sungbongju <sungbongju1@gmail.com>
2026-01-15 01:49:35 +0900

    Update route.ts

--- app/api/get-access-token/route.ts
+++ app/api/get-access-token/route.ts
@@ -5,25 +5,24 @@ export async function POST() {
     if (!HEYGEN_API_KEY) {
       throw new Error("API key is missing from .env");
     }
-    const baseApiUrl = process.env.NEXT_PUBLIC_BASE_API_URL;
-
+    
+    const baseApiUrl = process.env.NEXT_PUBLIC_BASE_API_URL || "https://api.heygen.com";
+    
     const res = await fetch(`${baseApiUrl}/v1/streaming.create_token`, {
       method: "POST",
       headers: {
         "x-api-key": HEYGEN_API_KEY,
       },
     });
-
+    
     console.log("Response:", res);
-
     const data = await res.json();
-
+    
     return new Response(data.data.token, {
       status: 200,
     });
   } catch (error) {
     console.error("Error retrieving access token:", error);
-
     return new Response("Failed to retrieve access token", {
       status: 500,
     });
